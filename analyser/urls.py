0eab0c000dc3e411698f025be36cda413e6487ad
Yash Verma <verma.yash8@gmail.com>
2017-03-15 19:15:45 +0530

    All templates and views added

--- analyser/urls.py
+++ analyser/urls.py
@@ -5,5 +5,6 @@ app_name = 'analyser'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^upload/$', views.upload_file, name='upload')
+    url(r'^upload/$', views.upload_file, name='upload'),
+    url(r'^features/', views.features_home, name='feature_home'),
 ]
