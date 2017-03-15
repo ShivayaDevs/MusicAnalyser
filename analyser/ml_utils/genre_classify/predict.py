a57b0b577d7af1124737cbf004a4975fc50008a5
shilpi75 <shilpi.c75@gmail.com>
2017-03-15 20:24:13 +0530

    Genre classification added

---
+++ analyser/ml_utils/genre_classify/predict.py
@@ -0,0 +1,45 @@
+import os
+import sys
+import scipy.io.wavfile
+import numpy as np
+from scikits.talkbox.features import mfcc
+#from classify import predict_file
+from pydub import AudioSegment
+from sklearn.externals import joblib
+
+def predict_song(path) :
+	
+	#convert to wav
+	#os.system("sox "+str(path)+" " + str(path[:-3]) + ".wav")
+	song = AudioSegment.from_file(path)
+	song = song[:30000]
+	song.export(path[:-3]+"wav",format='wav')
+	
+	#ceps
+
+	base_wav, ext = os.path.splitext(path)
+	wavfile = base_wav + ".wav"
+	sampling_rate, song_array = scipy.io.wavfile.read(wavfile)
+	song_array[song_array==0]=1
+	ceps, mspec, spec = mfcc(song_array)
+	base_wav, ext = os.path.splitext(wavfile)
+	data_wav = base_wav + ".ceps"
+	np.save(data_wav, ceps)
+	
+	#features
+	X = []
+	Y = []
+	ceps = np.load(data_wav+".npy")
+	num_ceps = len(ceps)
+	X.append(np.mean(ceps[int(num_ceps * 1/10) : int(num_ceps * 9/10)], axis = 0))
+
+	#prediction
+	#print predict_file(X)
+	genre_list = [ "country", "hiphop", "metal", "pop", "reggae", "rock"]
+
+	clf = joblib.load('./model_ceps.pkl')
+	index = clf.predict(X)
+	return genre_list[index[0]]
+	
+
+
