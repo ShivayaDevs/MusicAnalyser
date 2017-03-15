1eeed47fc83488021690da0e719216fac24ab500
Yash Verma <verma.yash8@gmail.com>
2017-03-15 19:07:09 +0530

    File upload is now working

--- analyser/urls.py
+++ analyser/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 import views
 
+app_name = 'analyser'
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^upload/$', views.upload_file, name='upload')
 ]
--- analyser/views.py
+++ analyser/views.py
@@ -1,6 +1,23 @@
+from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
     return render(request, 'analyser/index.html', {})
+
+
+def upload_file(request):
+    if request.method == 'POST' and request.FILES['music-input']:
+        audio_file = request.FILES['music-input']
+        fs = FileSystemStorage()
+        filename = fs.save(audio_file.name, audio_file)
+        uploaded_file_url = fs.url(filename)
+
+        return render(request, 'analyser/success.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+    return render(request, 'analysis/success.html')
+
+
+def features_home(request):
+    return render(request, 'analyser/feature_home.html', {})
